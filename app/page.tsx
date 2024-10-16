d501100e75c0ba15e9bd6d68f652503cfde1e82c
Hubert <hubert@relikowski.dev>
2024-10-16 15:39:01 +0200

    Passkey function and admin login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className=" flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
       <section className="flex remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -21,7 +26,7 @@ export default function Home() {
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 CarePulse copyright
             </p>
-            <Link href="/admin=true" className="text-green-500">
+            <Link href="?admin=true" className="text-green-500">
               Admin
             </Link>
           </div>
